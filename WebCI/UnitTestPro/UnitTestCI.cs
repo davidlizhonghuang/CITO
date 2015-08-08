4408c9bc6dfb3ce41d16d65057a7a46955d2fba9
david huang <clousoversea@gmail.com>
2015-08-09 07:54:18 +1000

    add unit tet

---
+++ WebCI/UnitTestPro/UnitTestCI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestPro
+{
+    [TestClass]
+    public class UnitTestCI
+    {
+        [TestMethod]
+        public void CItest()
+        {
+            var num = 5;
+
+            Assert.AreEqual(5, num);
+        }
+    }
+}
